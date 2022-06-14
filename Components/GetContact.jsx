1541da71919ec0b35f72d69210323314c325ec58
krisnasudiatmika <kresnasudiatmika@gmail.com>
2022-06-14 13:44:23 +0800

    crud complete

--- Components/GetContact.jsx
+++ Components/GetContact.jsx
@@ -1,15 +1,16 @@
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, TextInput } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, TextInput, Modal } from "react-native";
 import { useState, useEffect } from 'react'
 import fetchClient from './services/axios'
 const GetContacts = () => {
   const url = "https://gorest.co.in/public/v2/users";
-
+  const [status, setStatus] = useState('post');
   const [listUser, setListUser] = useState([])
   const [newUser, setNewUser] = useState({
     name: '',
     gender: '',
     email: '',
-    status: ''
+    status: '',
+    id: ''
   })
 
   //unntuk memanggil liset user
@@ -22,22 +23,91 @@ const GetContacts = () => {
     }
   };
 
+  const getUserDetail = async (id) => {
+    try {
+      const response = await fetchClient.get(url + `/${id}`);
+      if (response) {
+        const { email, gender, id, name, status } = response.data;
+        setNewUser({
+          name: name,
+          gender: gender,
+          email: email,
+          status: status,
+          id: id
+        });
+        setStatus('update')
+      }
+    } catch (error) {
+      console.log('Data fetching cancelled');
+    }
+  }
+
+  const deleteUser = async (id) => {
+    try {
+      const response = await fetchClient.delete(url + `/${id}`);
+      if (response) {
+        console.log(response)
+        const { email, gender, id, name, status } = response.data;
+        setNewUser({
+          name: name,
+          gender: gender,
+          email: email,
+          status: status,
+          id: id
+        });
+        getUsers()
+      }
+    } catch (error) {
+      console.log('Data fetching cancelled');
+    }
+  }
+
+
+
   //untuk menginput data
   const postUsers = async () => {
     try {
       const response = await fetchClient.post(url, newUser);
       if (response) {
         getUsers();
+        setNewUser({
+          name: '',
+          gender: '',
+          email: '',
+          status: '',
+          id: ''
+        });
       }
     } catch (error) {
       console.log('Data fetching cancelled', error.response);
     }
   };
 
+  const updateUser = async () => {
+    try {
+      const response = await fetchClient.put(url + `/${newUser.id}`, newUser);
+      if (response) {
+        getUsers();
+      }
+    } catch (error) {
+      console.log('Data fetching cancelled', error.response);
+    }
+  }
+
   useEffect(() => {
     getUsers();
   }, []);
 
+  const clear = () => {
+    setNewUser({
+      name: '',
+      gender: '',
+      email: '',
+      status: '',
+      id: ''
+    });
+    setStatus('post')
+  }
 
 
 
@@ -46,39 +116,49 @@ const GetContacts = () => {
 
       {listUser.length > 0 && listUser.map(user => {
         return (
-          <View style={styles.listContainer}>
-            <Text>{user.name}</Text>
+          <View style={styles.hapus}>
+            <TouchableOpacity style={styles.listContainer} onPress={() => getUserDetail(user.id)}>
+              <Text>{user.name}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => deleteUser(user.id)}>
+              <Text>X</Text>
+            </TouchableOpacity>
           </View>
+
         )
       })}
 
       <View style={styles.textInputContainer}>
-        <TextInput placeholder="Name" style={styles.textInput} onChangeText={val => {
+        <TextInput placeholder="Name" style={styles.textInput} value={newUser.name} onChangeText={val => {
           setNewUser(prev => {
             return { ...prev, name: val };
           })
         }} />
-        <TextInput placeholder="gender" style={styles.textInput} onChangeText={val => {
+        <TextInput placeholder="gender" style={styles.textInput} value={newUser.gender} onChangeText={val => {
           setNewUser(prev => {
             return { ...prev, gender: val };
           })
         }} />
-        <TextInput placeholder="email" style={styles.textInput} onChangeText={val => {
+        <TextInput placeholder="email" style={styles.textInput} value={newUser.email} onChangeText={val => {
           setNewUser(prev => {
             return { ...prev, email: val };
           })
         }} />
-        <TextInput placeholder="status" style={styles.textInput} onChangeText={val => {
+        <TextInput placeholder="status" style={styles.textInput} value={newUser.status} onChangeText={val => {
           setNewUser(prev => {
             return { ...prev, status: val };
           })
         }} />
       </View>
 
-      <TouchableOpacity style={styles.button} onPress={() => postUsers()}>
-        <Text>Tambah Data</Text>
+      <TouchableOpacity style={styles.button} onPress={() => status == 'post' ? postUsers() : updateUser()}>
+        <Text>Submit</Text>
       </TouchableOpacity>
-    </ScrollView>
+      <TouchableOpacity style={styles.button} onPress={() => clear()}>
+        <Text>Cancel</Text>
+      </TouchableOpacity>
+
+    </ScrollView >
   );
 }
 
@@ -98,11 +178,16 @@ const styles = StyleSheet.create({
   listContainer: {
     padding: 5,
     borderWidth: 1,
-    marginBottom: 3
+    marginBottom: 3,
+    width: '90%'
   },
   button: {
     backgroundColor: 'red',
     padding: 20,
     marginTop: 20
+  },
+  hapus: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 });
